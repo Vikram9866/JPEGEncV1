b44d6a94b75f8a359be4693269ac6fd4d0627727
Vikram raigur <vikram.purohith@students.iiit.ac.in>
2016-06-02 02:52:15 +0530

    Update RLEnew.py

--- RLEnew.py
+++ RLEnew.py
@@ -1,6 +1,7 @@
-from myhdl import *
+from myhdl import always_comb, always_seq, block, delay
+from myhdl import instance, intbv, ResetSignal, Signal
+from EntropyCoder import entropycoder
 from myhdl.conversion import *
-from EntropyCoder import *
 
 
 WIDTH_RAM_ADDRESS = 6
@@ -11,36 +12,27 @@ SIZE = 4
 def sub(a, b):
     return (a - b)
 
+
+
 @block
 def rle(reset, clock, di, start, sof, color_component, 
         runlength, size, amplitude, dovalid, read_addr):
 
+    prev_dc_0, prev_dc_1, prev_dc_2, zrl_di = [
+                            Signal(intbv(0)[WIDTH_RAM_DATA:].signed()) for _ in range(4)]
 
-    prev_dc_0 = Signal(intbv(0)[WIDTH_RAM_DATA:].signed())
-    prev_dc_1 = Signal(intbv(0)[WIDTH_RAM_DATA:].signed())
-    prev_dc_2 = Signal(intbv(0)[WIDTH_RAM_DATA:].signed())
+    accumulator, amplitude_temp = [
+                            Signal(intbv(0)[(WIDTH_RAM_DATA + 1):].signed()) for _ in range(2)]
 
-    accumulator = Signal(intbv(0)[(WIDTH_RAM_DATA + 1):])
+    size_temp, runlength_temp = [
+                            Signal(intbv(0)[SIZE:]) for _ in range(2)]
 
-    size_temp = Signal(intbv(0)[SIZE:])
-    amplitude_temp = Signal(intbv(0)[(WIDTH_RAM_DATA + 1):].signed())
-    runlength_temp = Signal(intbv(0)[4:]) 
-    
-    dovalid_temp = Signal(bool(0))
-    zero_cnt = Signal(intbv(0)[6:])
-    write_cnt_temp = Signal(intbv(0)[7:])
+    dovalid_temp, read_en, divalid, divalid_en, zrl_proc = [
+                            Signal(bool(0)) for _ in range(5)]
+
+    zero_cnt, read_cnt = [Signal(intbv(0)[6:]) for _ in range(2)]
     write_cnt = Signal(intbv(0)[7:])
     
-    read_cnt = Signal(intbv(0)[6:])
-    read_en = Signal(bool(0))
-    
-    divalid = Signal(bool(0))
-    divalid_en = Signal(bool(0))
-    
-    zrl_proc = Signal(bool(0))
-    zrl_di = Signal(intbv(0)[WIDTH_RAM_DATA:])
-
-
 
     @always_comb
     def assign():   
@@ -50,12 +42,11 @@ def rle(reset, clock, di, start, sof, color_component,
     
    
 
-    @always_seq(clock.posedge,reset = reset)
+    @always_seq(clock.posedge, reset = reset)
     def mainprocessing():
 
         dovalid_temp.next = 0
         runlength_temp.next = 0
-        write_cnt_temp.next = write_cnt
         runlength.next = runlength_temp
         dovalid.next = dovalid_temp
         divalid.next = read_en
@@ -67,8 +58,7 @@ def rle(reset, clock, di, start, sof, color_component,
 
         if divalid == 1 and write_cnt == 63:
             divalid_en.next = 0         
-    
-        #input read enable
+
         if read_en == 1:  
             if read_cnt == 63:
                 read_cnt.next = 0
@@ -78,7 +68,6 @@ def rle(reset, clock, di, start, sof, color_component,
 
         if divalid == 1:
             write_cnt.next = write_cnt + 1
-
 
             if write_cnt == 0:
                 
@@ -119,8 +108,8 @@ def rle(reset, clock, di, start, sof, color_component,
                         write_cnt.next = 0
 
                     if zero_cnt <= 15:
-                        accumulator.next = di[WIDTH_RAM_DATA:0].signed()
-                        runlength_temp.next = zero_cnt[4:0] 
+                        accumulator.next = di
+                        runlength_temp.next = zero_cnt
                         zero_cnt.next = 0
                         dovalid_temp.next = 1
 
@@ -139,8 +128,8 @@ def rle(reset, clock, di, start, sof, color_component,
         if zrl_proc == 1:
             if zero_cnt <= 15:
                 
-                accumulator.next = zrl_di[WIDTH_RAM_DATA+1:0].signed() 
-                runlength_temp.next = zero_cnt[4:0]
+                accumulator.next = zrl_di
+                runlength_temp.next = zero_cnt
 
                 if zrl_di.signed() == 0:
                     zero_cnt.next = 1
@@ -168,11 +157,11 @@ def rle(reset, clock, di, start, sof, color_component,
             prev_dc_1.next = 0
             prev_dc_2.next = 0
 
-    en_coder = entropycoder(clock, reset, accumulator, size_temp, amplitude_temp)
+    encoder_inst = entropycoder(clock, reset, accumulator, size_temp, amplitude_temp)
 
 
 
-    return assign, mainprocessing, en_coder
+    return assign, mainprocessing, encoder_inst
 
 
 
@@ -180,24 +169,31 @@ def rle(reset, clock, di, start, sof, color_component,
 
 def convert():
 
-    #Signal declaration
     clock = Signal(bool(0))
     reset = ResetSignal(0, active=1, async=True)
+
+    
     di = Signal(intbv(0)[WIDTH_RAM_DATA:])
     start = Signal(bool(0))
-    sof = Signal(bool(0))
     color_component = Signal(intbv(0)[3:])
+
+
     runlength = Signal(intbv(0)[4:])
     size = Signal(intbv(0)[4:])
     amplitude = Signal(intbv(0)[WIDTH_RAM_DATA:])
+    
     dovalid = Signal(bool(0))
+    sof = Signal(bool(0))
+
     read_addr = Signal(intbv(0)[6:])
     wr_addr = Signal(intbv(0)[7:])
 
-    jpgv1 = rle(reset, clock, di, start, sof, color_component, runlength, size, amplitude, dovalid, read_addr)
+    inst = rle(reset, clock, di, start, sof, color_component, 
+        runlength, size, amplitude, dovalid, read_addr)
    
     analyze.simulator = 'iverilog'
-    assert rle(reset, clock, di, start, sof, color_component, runlength, size, amplitude, dovalid, read_addr).analyze_convert() == 0
+    assert rle(reset, clock, di, start, sof, color_component, 
+        runlength, size, amplitude, dovalid, read_addr).analyze_convert() == 0
 
 if __name__ == '__main__':
     convert()
