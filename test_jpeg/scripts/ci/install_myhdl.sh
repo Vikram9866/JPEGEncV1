8b42af90b2f924f2761cf3086d5de70e801753c2
vikram9866 <vikram.purohith@students.iiit.ac.in>
2016-06-17 00:29:13 +0530

    removed

--- test_jpeg/scripts/ci/install_myhdl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git clone https://github.com/jandecaluwe/myhdl
-cd myhdl
-python setup.py install
-make -C cosimulation/icarus
-# copy the VPI to the test directory
-cp cosimulation/icarus/myhdl.vpi ../test/
